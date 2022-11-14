00b4146fc05b79937e0c76584772b04a3adc8056
Joycita <117690379+Joycita@users.noreply.github.com>
2022-11-14 16:16:29 -0500

    Tarea de Clasesita 004-2

--- Clase04/Clase04_while.sh
+++ Clase04/Clase04_while.sh
@@ -2,8 +2,7 @@
 
 ##1. How many times were the levels of individuals 3 and 27 recorded?##
 echo "1. How many times were the levels of individuals 3 and 27 recorded?" 
-cd ../../../
-cd unix/data
+cd ../../../../../unix/data
 ls
 head -n Guesquiere2011_data.csv
 cut -f 1 Guesquiere2011_data.csv | head -n 4
