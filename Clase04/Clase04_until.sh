1f052553c553481adefc84e172cf991c9a482046
Joycita <117690379+Joycita@users.noreply.github.com>
2022-11-14 20:58:40 -0500

    Tarea de Clasesita 004-3

--- Clase04/Clase04_until.sh
+++ Clase04/Clase04_until.sh
@@ -1,20 +1,22 @@
 ## Ejercicio 1.10.3 Plant-Pollinator Networks ##
 ##1.Write a script that takes one of these files and determines the number of rows (pollinators) and columns (plants)##
 echo " 1. Write a script that takes one of these files and determines the number of rows (pollinators) and columns (plants)"
-cd ../../../../../unix/data/Saavedra2013
-ls -x
-bash n5.txt
-cat n1.txt | wc -l
-cat n59.txt | wc -l
+cd ../../../../../unix/data
+wc -l Saavedra2013/n1.txt 
+
+cat Saavedra2013/n1.txt | wc -l
+cat Saavedra2013/n59.txt | wc -l
 
 echo -e "\n"
+head -n 1 Saavedra2013/n1.txt
+head -n 1 Saavedra2013/n59.txt
 
-head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c
-head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c
+head -n 1 Saavedra2013/n1.txt | tr -d ' ' | tr -d '\n' | wc -c
+head -n 1 Saavedra2013/n59.txt | tr -d ' ' | tr -d '\n' | wc -c
 
 ##2.[Advanced]Write a script that prints the numbers of rows and columns for each network:##
 echo "[Advanced]Write a script that prints the numbers of rows and columns for each network:"
-Files=*.txt
+Files=Saavedra2013/*.txt
 
 for c in $Files
 do
