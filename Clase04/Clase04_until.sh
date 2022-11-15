9ca72b083f85c09c56b1273f76b779f6e97d7ec5
Joycita <117690379+Joycita@users.noreply.github.com>
2022-11-14 22:44:58 -0500

    Tarea de Clasesita 004-3

--- Clase04/Clase04_until.sh
+++ Clase04/Clase04_until.sh
@@ -2,33 +2,36 @@
 ##1.Write a script that takes one of these files and determines the number of rows (pollinators) and columns (plants)##
 echo " 1. Write a script that takes one of these files and determines the number of rows (pollinators) and columns (plants)"
 cd ../../../../../unix/data
-wc -l Saavedra2013/n1.txt 
 
+echo "Para n1.txt"
 cat Saavedra2013/n1.txt | wc -l
-cat Saavedra2013/n59.txt | wc -l
-
-echo -e "\n"
-head -n 1 Saavedra2013/n1.txt
-head -n 1 Saavedra2013/n59.txt
-
 head -n 1 Saavedra2013/n1.txt | tr -d ' ' | tr -d '\n' | wc -c
+
+echo "Para n59.txt"
+cat Saavedra2013/n59.txt | wc -l
 head -n 1 Saavedra2013/n59.txt | tr -d ' ' | tr -d '\n' | wc -c
 
-##2.[Advanced]Write a script that prints the numbers of rows and columns for each network:##
-echo "[Advanced]Write a script that prints the numbers of rows and columns for each network:"
-Files=Saavedra2013/*.txt
 
-for c in $Files
+##2.[Advanced]Write a script that prints the numbers of rows and columns for each network##
+echo "[Advanced]Write a script that prints the numbers of rows and columns for each network:"
+
+for z in Saavedra2013/*.txt;
 do
-	Filitas=`cat $c | wc -l`
-	Columnitas=`head -n 1 $l |tr -d ' ' | tr -d '\n' | wc -c`
-	echo "Archivos:" $l "Filas:" $Filitas "Columnas:" $Columnitas
+	Filitas=`cat $z | wc -l`;
+	echo "Archivos:" $z "Filitas:" $Filitas;
+done
+
+for z in Saavedra2013/*.txt;
+do 
+	Columnitas=`head -n 1 $z | tr -d ' ' | tr -d '\n' | wc -c`;
+	echo "Archivos:" $z "Columnitas:" $Columnitas;
 done
 
 ##3.Which file has the largest number of rows? Which has the largest number of columns?##
 echo "Which file has the largest number of rows? Which has the largest number of columns?"
+
 echo "Mayor número de columnas"
-bash netsize_all.sh  | sort -n -r -k 3 | head -n 1
+bash  ../../Todaslascositas.sh | sort -n -r -k 3 | head -n 1
 
 echo "Mayor número de filas"
-bash netsize_all.sh | sort -n -r -k 2 | head -n 1
+bash  ../../Todaslascositas.sh | sort -n -r -k 2 | head -n 1
